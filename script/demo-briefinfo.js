419fde321c560a7292ad81a49445a858c47949ec
WhyPayIt <zxwshai@gmail.com>
2016-04-24 00:12:18 +0800

    Class Final Demo.
    Class Final Demo.

---
+++ script/demo-briefinfo.js
@@ -0,0 +1,20 @@
+var divboxOne =
+    document.getElementsByClassName('born_place')[0];
+
+divboxOne.onclick = function() {
+    divboxOne.classList.add('demo');
+};
+
+var divboxTwo =
+    document.getElementsByClassName('student')[0];
+
+divboxTwo.onclick = function() {
+    divboxTwo.classList.add('demo');
+};
+
+var divboxThree =
+    document.getElementsByClassName('work')[0];
+
+divboxThree.onclick = function() {
+    divboxThree.classList.add('demo');
+};
---
+++ script/demo-reviews.js
@@ -0,0 +1,7 @@
+$(function() {
+    $("li").click(function() {
+        //不要點了其中一個list就show全部
+        //$("li").addClass("demo");
+        $(this).addClass("demo");
+    });
+});
